adde8da12cdbb8a9c2b24bec207fcb545f524532
Vladislav <senior.vlados97@gmail.com>
2023-12-26 21:42:07 +0300

    create own callback

--- src/Components/Flights/Flights.js
+++ src/Components/Flights/Flights.js
@@ -1,31 +1,62 @@
-import React, { useState, useMemo, useRef } from "react";
+import React, { useState, useMemo, useRef, useCallback } from "react";
 import axios from "axios";
 import EditToolBar from "../EditToolBar";
-import { DataGrid, GridActionsCellItem, GridRowModes, GridRowEditStopReasons } from "@mui/x-data-grid";
+import {
+    DataGrid,
+    GridActionsCellItem,
+    GridRowModes,
+    GridRowEditStopReasons,
+} from "@mui/x-data-grid";
 import { useLoaderData } from "react-router-dom";
-import EditIcon from '@mui/icons-material/Edit';
+import EditIcon from "@mui/icons-material/Edit";
 import SaveIcon from "@mui/icons-material/Save";
 import CancelIcon from "@mui/icons-material/Cancel";
 import DeleteIcon from "@mui/icons-material/Delete";
 import style from "../style";
 
 export const loadRows = async () => {
-    const urls = ["http://localhost:8080/flight/get", "http://localhost:8080/vehicle/get", "http://localhost:8080/airport/get"];
+    const urls = [
+        "http://localhost:8080/flight/get",
+        "http://localhost:8080/vehicle/get",
+        "http://localhost:8080/airport/get",
+    ];
     const responses = Promise.all(urls.map((url) => axios.get(url)));
     return responses;
 };
 
-export const saveRow = async (row) => {
-    await axios.post("http://localhost:8080/flight/create", row);
+// const saveRow = async (row) => {
+//     await axios.post("http://localhost:8080/flight/create", row);
+// };
+
+const useSaveRow = () => {
+    return React.useCallback(
+        (row) =>
+            new Promise((resolve, reject) => {
+                try {
+                    const response = axios.post(
+                        "http://localhost:8080/flight/create",
+                        row
+                    );
+                    resolve(response.data);
+                } catch (err) {
+                    reject(
+                        new Error(
+                            "Ошибка при сохранении: заполните обязательные поля"
+                        )
+                    );
+                }
+            }),
+        []
+    );
 };
 
-export const updateRow = async (row) => {
+const updateRow = async (row) => {
     await axios.put(`http://localhost:8080/flight/update/${row.id}`, row);
 };
 
-export const deleteRow = async (id) => {
+const deleteRow = async (id) => {
     await axios.delete(`http://localhost:8080/flight/delete/${id}`);
-}
+};
 
 const Flights = () => {
     const response = useLoaderData();
@@ -33,6 +64,7 @@ const Flights = () => {
     const [rowModesModel, setRowModesModel] = useState({});
     const vehicles = useRef(response[1].data);
     const airports = useRef(response[2].data);
+    const saveRow = useSaveRow();
 
     const handleRowEditStop = (params, event) => {
         if (params.reason === GridRowEditStopReasons.rowFocusOut) {
@@ -41,11 +73,17 @@ const Flights = () => {
     };
 
     const handleEditClick = (id) => () => {
-        setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.Edit } });
+        setRowModesModel({
+            ...rowModesModel,
+            [id]: { mode: GridRowModes.Edit },
+        });
     };
 
     const handleSaveClick = (id) => () => {
-        setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.View } });
+        setRowModesModel({
+            ...rowModesModel,
+            [id]: { mode: GridRowModes.View },
+        });
     };
 
     const handleDeleteClick = (id) => () => {
@@ -54,7 +92,10 @@ const Flights = () => {
     };
 
     const handleCancelClick = (id) => () => {
-        setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.View, ignoreModifications: true }, });
+        setRowModesModel({
+            ...rowModesModel,
+            [id]: { mode: GridRowModes.View, ignoreModifications: true },
+        });
 
         const editedRow = rows.find((row) => row.id === id);
         if (editedRow.isNew) {
@@ -62,24 +103,41 @@ const Flights = () => {
         }
     };
 
-    const processRowUpdate = (newRow) => {
-        const vehicle = vehicles.current.find((v) => v.id === newRow.vehicle);
-        const airportFrom = airports.current.find((a) => a.id === newRow.from);
-        const airportTo = airports.current.find((a) => a.id === newRow.to);
-        newRow.vehicle = vehicle;
-        newRow.from = airportFrom;
-        newRow.to = airportTo;
+    // const processRowUpdate = (newRow) => {
+    //     const vehicle = vehicles.current.find((v) => v.id === newRow.vehicle);
+    //     const airportFrom = airports.current.find((a) => a.id === newRow.from);
+    //     const airportTo = airports.current.find((a) => a.id === newRow.to);
+    //     newRow.vehicle = vehicle;
+    //     newRow.from = airportFrom;
+    //     newRow.to = airportTo;
 
-        if (newRow.isNew === undefined || !newRow.isNew) {
-            updateRow(newRow);
-        } else {
-            saveRow(newRow);
-        }
+    //     if (newRow.isNew === undefined || !newRow.isNew) {
+    //         updateRow(newRow);
+    //     } else {
+    //         saveRow(newRow);
+    //     }
 
-        const updatedRow = { ...newRow, isNew: false };
-        setRows(rows.map((row) => (row.id === newRow.id ? updatedRow : row)));
-        return updatedRow;
-    };
+    //     const updatedRow = { ...newRow, isNew: false };
+    //     setRows(rows.map((row) => (row.id === newRow.id ? updatedRow : row)));
+    //     return updatedRow;
+    // };
+
+    const processRowUpdate = useCallback(
+        async (newRow) => {
+            const response = await saveRow(newRow);
+            const vehicle = vehicles.current.find(
+                (v) => v.id === newRow.vehicle
+            );
+            const airportFrom = airports.current.find(
+                (a) => a.id === newRow.from
+            );
+            const airportTo = airports.current.find((a) => a.id === newRow.to);
+            newRow.vehicle = vehicle;
+            newRow.from = airportFrom;
+            newRow.to = airportTo;
+        },
+        [saveRow]
+    );
 
     const handleProcessRowUpdateError = () => {
         alert("error");
@@ -91,9 +149,12 @@ const Flights = () => {
 
     const columns = [
         {
-            field: "actions", headerName: "Действия", type: "actions",
+            field: "actions",
+            headerName: "Действия",
+            type: "actions",
             getActions: ({ id }) => {
-                const isInEditMode = rowModesModel[id]?.mode === GridRowModes.Edit;
+                const isInEditMode =
+                    rowModesModel[id]?.mode === GridRowModes.Edit;
 
                 if (isInEditMode) {
                     return [
@@ -101,7 +162,7 @@ const Flights = () => {
                             icon={<SaveIcon />}
                             label="Save"
                             sx={{
-                                color: 'primary.main',
+                                color: "primary.main",
                             }}
                             onClick={handleSaveClick(id)}
                         />,
@@ -132,43 +193,67 @@ const Flights = () => {
                 ];
             },
         },
-        { field: "id", headerName: "ID", width: "25", editable: "true", },
+        { field: "id", headerName: "ID", width: "25", editable: "true" },
         {
-            field: "num", headerName: "Номер", width: "100", type: "number", editable: "true",
+            field: "num",
+            headerName: "Номер",
+            width: "100",
+            type: "number",
+            editable: "true",
             preProcessEditCellProps: (params) => {
-                const hasError = params.props.value === undefined || params.props.value === "";
+                const hasError =
+                    params.props.value === undefined ||
+                    params.props.value === "";
                 return { ...params.props, error: hasError };
-            }
+            },
         },
         {
-            field: "from", headerName: "Место отправления", width: "200", type: "singleSelect", editable: "true",
+            field: "from",
+            headerName: "Место отправления",
+            width: "200",
+            type: "singleSelect",
+            editable: "true",
             valueGetter: (option) => {
-                const value = option?.value?.id === undefined ? "" : option?.value?.id;
+                const value =
+                    option?.value?.id === undefined ? "" : option?.value?.id;
                 return value;
             },
             valueOptions: () => {
                 const options = [];
                 for (let i = 0; i < airports.current.length; i++) {
-                    options.push({ value: airports.current[i].id, label: `${airports.current[i].name} (${airports.current[i].abbreviation})` })
+                    options.push({
+                        value: airports.current[i].id,
+                        label: `${airports.current[i].name} (${airports.current[i].abbreviation})`,
+                    });
                 }
                 return options;
             },
             preProcessEditCellProps: (params) => {
                 const to = params.otherFieldsProps.to.value;
-                const hasError = params.props.value === to || params.props.value === undefined;
+                const hasError =
+                    params.props.value === to ||
+                    params.props.value === undefined;
                 return { ...params.props, error: hasError };
             },
         },
         {
-            field: "to", headerName: "Место прибытия", width: "200", type: "singleSelect", editable: "true",
+            field: "to",
+            headerName: "Место прибытия",
+            width: "200",
+            type: "singleSelect",
+            editable: "true",
             valueGetter: (option) => {
-                const value = option?.value?.id === undefined ? "" : option?.value?.id;
+                const value =
+                    option?.value?.id === undefined ? "" : option?.value?.id;
                 return value;
             },
             valueOptions: () => {
                 const options = [];
                 for (let i = 0; i < airports.current.length; i++) {
-                    options.push({ value: airports.current[i].id, label: `${airports.current[i].name} (${airports.current[i].abbreviation})` })
+                    options.push({
+                        value: airports.current[i].id,
+                        label: `${airports.current[i].name} (${airports.current[i].abbreviation})`,
+                    });
                 }
                 return options;
             },
@@ -179,27 +264,43 @@ const Flights = () => {
             },
         },
         {
-            field: "departureDate", headerName: "Дата отправления", width: "200", type: "dateTime", editable: "true",
+            field: "departureDate",
+            headerName: "Дата отправления",
+            width: "200",
+            type: "dateTime",
+            editable: "true",
             valueGetter: (params) => {
                 return new Date(params.value);
             },
         },
         {
-            field: "arrivingDate", headerName: "Дата прибытия", width: "200", type: "dateTime", editable: "true",
+            field: "arrivingDate",
+            headerName: "Дата прибытия",
+            width: "200",
+            type: "dateTime",
+            editable: "true",
             valueGetter: (params) => {
                 return new Date(params.value);
-            }
+            },
         },
         {
-            field: "vehicle", headerName: "ТС", width: "200", type: "singleSelect", editable: "true",
+            field: "vehicle",
+            headerName: "ТС",
+            width: "200",
+            type: "singleSelect",
+            editable: "true",
             valueGetter: (option) => {
-                const value = option?.value?.id === undefined ? '' : option?.value?.id;
+                const value =
+                    option?.value?.id === undefined ? "" : option?.value?.id;
                 return value;
             },
             valueOptions: () => {
                 const options = [];
                 for (let i = 0; i < vehicles.current.length; i++) {
-                    options.push({ value: vehicles.current[i].id, label: vehicles.current[i].type });
+                    options.push({
+                        value: vehicles.current[i].id,
+                        label: vehicles.current[i].type,
+                    });
                 }
                 return options;
             },
@@ -209,7 +310,7 @@ const Flights = () => {
     const columnVisibilityModel = useMemo(() => {
         return {
             id: false,
-        }
+        };
     }, []);
 
     return (
@@ -230,7 +331,7 @@ const Flights = () => {
                 initialState={{
                     pagination: {
                         paginationModel: { page: 0, pageSize: 10 },
-                    }
+                    },
                 }}
                 pageSizeOptions={[10]}
                 checkboxSelection={false}
@@ -241,9 +342,8 @@ const Flights = () => {
                     //columnsPanel: { getTogglableColumns },
                 }}
             />
-
         </>
     );
-}
+};
 
 export default Flights;
