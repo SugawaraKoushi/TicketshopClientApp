be5e53a09fbdd6b1a67433500c45ec9f9e3d861e
Vladek <senior.vlados97@gmail.com>
2023-12-29 20:47:32 +0300

    generating prices for tickets

--- src/Components/Flights/FoundFlights.js
+++ src/Components/Flights/FoundFlights.js
@@ -8,13 +8,23 @@ import FlightLandIcon from '@mui/icons-material/FlightLand';
 const FoundFlights = () => {
     const { state } = useLocation();
     const [flights, setFlights] = useState([]);
+    const [prices, setPrices] = useState([]);
+    const [luggagePrices, setLuggagePrices] = useState([]);
 
     useEffect(() => {
         fetchData();
     }, []);
 
     const fetchData = async () => {
-        await axios("http://localhost:8080/flight/getWhenDate", { params: state }).then((response) => { setFlights(response.data) });
+        try {
+            const response = await axios.get("http://localhost:8080/flight/getWhenDate", { params: state });
+            const data = response.data;
+            setFlights(data);
+            generatePrice(data.length);
+            generateLuggagePrice(data.length);
+        } catch (err) {
+            alert(err.message);
+        }
     };
 
     const calculateTimeDifference = (departure, arriving) => {
@@ -25,36 +35,74 @@ const FoundFlights = () => {
         return diffInHours;
     };
 
+    const generatePrice = (size) => {
+        const min = 2500;
+        const max = 10000;
+        let prices = [];
+        for (let i = 0; i < size; i++) {
+            const price = Math.random() * (max - min) + min;
+            prices.push(price);
+        };
+        setPrices(prices);
+    };
+
+    const generateLuggagePrice = (size) => {
+        const min = 500;
+        const max = 2500;
+        for (let i = 0; i < size; i++) {
+            const price = Math.random() * (max - min) + min;
+            setLuggagePrices([...luggagePrices, price.toFixed(0)]);
+        }
+    }
+
+    const getLuggageLabel = (i) => {
+        return `Багаж +${luggagePrices[i]}`;
+    };
+
+    const getDate = (date) => {
+
+    };
+
     return (
         <>
-            {flights.map((flight) => (
+            {flights.map((flight, i) => (
                 <Container fluid sx={{ margin: "auto", width: "45%", padding: "20px" }}>
                     <Box sx={{ display: "flex", width: "100%", border: "1px solid", borderColor: "rgb(217, 217, 217)", borderRadius: "15px" }} id="flight-side-content">
                         <Box sx={{ padding: "5px", display: "flex", flexDirection: "column", alignContent: "space-between", width: "25%", height: "150px", borderRight: "1px solid", borderColor: "rgb(217, 217, 217)" }}>
-                            <InputLabel id="price-label" sx={{ textAlign: "center", fontSize: "18pt" }}>6000 ₽</InputLabel>
+                            <InputLabel id="price-label" sx={{ textAlign: "center", fontSize: "18pt" }}>{prices[i]} ₽</InputLabel>
                             <FormControlLabel
                                 sx={{ margin: "auto", width: "80%", border: "1px solid", borderColor: "rgb(217, 217, 217)", borderRadius: "5px" }}
                                 control={<Switch defaultChecked />}
-                                label="Багаж +2500"
+                                label={getLuggageLabel(i)}
                                 labelPlacement="start"
                             />
                             <Button variant="outlined" sx={{ width: "80%", margin: "auto" }}>Выбрать</Button>
                         </Box>
                         <Grid container sx={{ width: "75%", padding: "5px", alignContent: "center" }}>
                             <Grid item xs={2}>
-                                <InputLabel sx={{ fontSize: "18pt", textAlign: "center" }}>{new Date(flight.departureDate).getHours()}:{new Date(flight.departureDate).getMinutes()}</InputLabel>
-                                <InputLabel sx={{ fontSize: "8pt", textAlign: "left" }}>{flight.from.city.name}<br />1 янв, пн</InputLabel>
+                                <InputLabel sx={{ fontSize: "18pt", textAlign: "center" }}>
+                                    {new Date(flight.departureDate).getHours()}:{new Date(flight.departureDate).getMinutes()}
+                                </InputLabel>
+                                <InputLabel sx={{ fontSize: "8pt", textAlign: "left" }}>
+                                    {flight.from.city.name}<br />1 янв, пн
+                                </InputLabel>
                             </Grid>
                             <Grid item xs={8} sx={{ borderBottom: "solid", borderColor: "rgb(217, 217, 217)", height: "25%" }}>
                                 <Box sx={{ display: "flex", justifyContent: "space-between" }}>
                                     <FlightTakeoffIcon color="disabled" />
-                                    <InputLabel sx={{ color: "rgb(217, 217, 217)" }}>В пути {calculateTimeDifference(flight.departureDate, flight.arrivingDate)} часа</InputLabel>
+                                    <InputLabel sx={{ color: "rgb(217, 217, 217)" }}>
+                                        В пути {calculateTimeDifference(flight.departureDate, flight.arrivingDate)} часа
+                                    </InputLabel>
                                     <FlightLandIcon color="disabled" />
                                 </Box>
                             </Grid>
                             <Grid item xs={2}>
-                                <InputLabel sx={{ fontSize: "18pt", textAlign: "center" }}>{new Date(flight.arrivingDate).getHours()}:{new Date(flight.arrivingDate).getMinutes()}</InputLabel>
-                                <InputLabel sx={{ fontSize: "8pt", textAlign: "right" }}>{flight.to.city.name}<br />1 янв, пн</InputLabel>
+                                <InputLabel sx={{ fontSize: "18pt", textAlign: "center" }}>
+                                    {new Date(flight.arrivingDate).getHours()}:{new Date(flight.arrivingDate).getMinutes()}
+                                </InputLabel>
+                                <InputLabel sx={{ fontSize: "8pt", textAlign: "right" }}>
+                                    {flight.to.city.name}<br />1 янв, пн
+                                </InputLabel>
                             </Grid>
                         </Grid>
                     </Box>
